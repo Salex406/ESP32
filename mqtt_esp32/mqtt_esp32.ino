e5426559ef4973f32a7b106d4448cf547619d7c5
Salex406 <amzhar@spels.ru>
2020-02-11 00:33:41 +0300

    Update mqtt_esp32.ino

--- mqtt_esp32/mqtt_esp32.ino
+++ mqtt_esp32/mqtt_esp32.ino
@@ -1,13 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <stdlib.h>
 #include <PubSubClient.h>
+#include <Wire.h>
+
 
 #define STASSID "BestWLAN"
-#define STAPSK  "zte"
+#define STAPSK  "zte30kl7"
 
+#define sht_write 0x80 
+#define sht_read 0x81 
+#define sht_addr 0x40 
+#define sht_temp 0xe3
+#define sht_hum 0xe5
+ 
 
 IPAddress ip(255,255,255,255);
 String mac; 
+const int Pin1 = 16;
 
 const char* mqtt_server = "sandbox.rightech.io";
 WiFiClient espClient;
@@ -30,8 +39,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
     Serial.println("1");
+     digitalWrite(Pin1, HIGH); 
   } else {
     Serial.println("0");
+     digitalWrite(Pin1, LOW); 
   }
 
 }
@@ -57,10 +68,58 @@ void reconnect() {
   }
 }
 
+
+uint16_t readSensor(uint8_t command)
+{
+    uint16_t result;
+
+    Wire.beginTransmission(sht_addr); //begin
+    Wire.write(command);                     //send the pointer location
+    Wire.endTransmission();                  //end
+    
+    //Hang out while measurement is taken. 50mS max, page 4 of datasheet.
+    delay(55);
+
+    //Comes back in three bytes, data(MSB) / data(LSB) / Checksum
+    Wire.requestFrom(sht_addr, 3);
+    
+    //Wait for data to become available
+    int counter = 0;
+    while(Wire.available() < 3)
+    {
+        counter++;
+        delay(1);
+        if(counter > 100) return 1; //Error timout
+    }
+
+    //Store the result
+    result = ((Wire.read()) << 8);
+    result |= Wire.read();
+   
+    result &= ~0x0003;   // clear two low bits (status bits)
+    return result;
+}
+
+float GetTemperature(void)
+{
+    return (-46.85 + 175.72 / 65536.0 * (float)(readSensor(sht_temp)));
+}
+
+float GetHumidity(void)
+{
+    return (-6 + 125 / 65536.0 * (float)(readSensor(sht_hum)));
+}
+
 void setup() {
   srand(12);
   Serial.begin(115200);
   Serial.println("strting");
+  
+  Wire.begin();
+  
+  pinMode(Pin1, OUTPUT);
+  digitalWrite(Pin1, HIGH); 
+
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(STASSID, STAPSK);
@@ -68,7 +127,7 @@ void setup() {
     //Serial.print('.');
     delay(500);
   }
-  Serial.println("cnnectd");
+  Serial.println("WIFI cnnectd");
   Serial.println(WiFi.localIP());
   mac = WiFi.macAddress();
   Serial.println(mac);
@@ -89,18 +148,59 @@ void loop() {
   if (now - lastMsg > 5000) {
     lastMsg = now;
     ++value;
-     int temp = rand() % 10 + 18;
-  int hum = 100;
-  Serial.print("Publish message: ");
-    char msg[20];
-    sprintf(msg,"%d",temp);
- 
-    client.publish("base/state/temperature", msg);
-  
-    sprintf(msg,"%d",hum);
+     //int temp = rand() % 10 + 18;
+  //int hum = 100;
+    float temp = GetTemperature();
+    char dat[10];
+    sprintf(dat,"%f",temp);
+    Serial.println(dat);
+    client.publish("base/state/temperature", dat);
     
-    client.publish("base/state/humidity", msg);
+    float hum = GetHumidity();
+    sprintf(dat,"%f",hum);
+    Serial.println(dat);
+    Serial.println("Published");
+    //char msg[20];
+    //sprintf(msg,"%d",temp);
+ 
+  
+    //sprintf(msg,"%d",hum);
+    
+    client.publish("base/state/humidity", dat);
   }
   
 
+}
+
+void loop_() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No devices found\n");
+  }
+  else {
+    Serial.println("done!\n");
+  }
+  delay(5000);          
 }
